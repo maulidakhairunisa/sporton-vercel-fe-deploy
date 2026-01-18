4dab3a0c425b68ef44153bc1899039034834d50d
maulidakhairunisa <khairunisamaulida@gmail.com>
2026-01-18 19:25:56 +0700

    live session 2

--- app/(landing)/order-status/[id]/page.tsx
+++ app/(landing)/order-status/[id]/page.tsx
@@ -4,12 +4,12 @@ import OrderConfirmed from "../../components/order-status/order-confirmed";
 import OrderSubmitted from "../../components/order-status/order-submitted";
 
 const OrderStatus = () => {
-    const [isConfirmed, setIsConfirmed] = useState(true);
+    const [isConfirmed, setIsConfirmed] = useState(false);
 
     return (
         <main className="bg-gray-100 min-h-[80vh]">
             <div className="max-w-5xl mx-auto py-20">
-                <h1 className="text-5xl font-bold text-center mb-15 ">Order Status</h1>
+                <h1 className="text-5xl font-bold text-center -mb-11 ">Order Status</h1>
             </div>
             {isConfirmed ? <OrderConfirmed /> : <OrderSubmitted />}
         </main >
